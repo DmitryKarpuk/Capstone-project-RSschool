413daa98e77bcebe812ec3ea73483d58b700766c
dimakarpuk22 <dimakarpuk22@gmail.com>
2022-04-29 22:58:17 +0300

    Add mlflow

--- src/forest_ml/train.py
+++ src/forest_ml/train.py
@@ -2,12 +2,16 @@ from pathlib import Path
 import click
 from joblib import dump
 
+import mlflow
+import mlflow.sklearn
+
 import pandas as pd
 import numpy as np
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_validate
 
+mlflow.set_tracking_uri('http://localhost:5000')
 
 @click.command()
 @click.option(
@@ -63,31 +67,40 @@ def train(
     X = df.iloc[:, :-1]
     y = df.iloc[:, -1]
     scorring = ("accuracy", "f1_weighted", "roc_auc_ovr")
-    clf = RandomForestClassifier(
-        n_estimators=n_estimators,
-        random_state=random_state,
-        max_depth=max_depth,
-        min_samples_split=min_samples_split,
-        min_samples_leaf=min_samples_leaf,
-    )
-    cv_results = cross_validate(
-        clf, X, y, cv=cv, scoring=scorring, return_estimator=True
-    )
-    accuracy = cv_results["test_accuracy"].max()
-    f1 = cv_results["test_f1_weighted"].max()
-    roc_auc = cv_results["test_roc_auc_ovr"].max()
-    if key_metric == "accuracy":
-        estimator = cv_results["estimator"][
-            np.argmax(cv_results["test_accuracy"])
-        ]
-    elif key_metric == "f1":
-        estimator = cv_results["estimator"][
-            np.armax(cv_results["test_f1_weighted"])
-        ]
-    elif key_metric == "roc_auc":
-        estimator = cv_results["estimator"][
-            np.argmax(cv_results["test_roc_auc_ovr"])
-        ]
+    with mlflow.start_run():
+        clf = RandomForestClassifier(
+            n_estimators=n_estimators,
+            random_state=random_state,
+            max_depth=max_depth,
+            min_samples_split=min_samples_split,
+            min_samples_leaf=min_samples_leaf,
+        )
+        cv_results = cross_validate(
+            clf, X, y, cv=cv, scoring=scorring, return_estimator=True
+        )
+        accuracy = cv_results["test_accuracy"].max()
+        f1 = cv_results["test_f1_weighted"].max()
+        roc_auc = cv_results["test_roc_auc_ovr"].max()
+        if key_metric == "accuracy":
+            estimator = cv_results["estimator"][
+                np.argmax(cv_results["test_accuracy"])
+            ]
+        elif key_metric == "f1":
+            estimator = cv_results["estimator"][
+                np.argmax(cv_results["test_f1_weighted"])
+            ]
+        elif key_metric == "roc_auc":
+            estimator = cv_results["estimator"][
+                np.argmax(cv_results["test_roc_auc_ovr"])
+            ]
+        mlflow.log_param('n_estimators', n_estimators)
+        mlflow.log_param('max_depth', max_depth)
+        mlflow.log_param('min_samples_split', min_samples_split)
+        mlflow.log_param('min_samples_leaf', min_samples_leaf)
+        mlflow.log_metric('accuracy', accuracy)
+        mlflow.log_metric('f1', f1)
+        mlflow.log_metric('roc_auc', roc_auc)
+        mlflow.sklearn.log_model(estimator, 'model')
     click.echo(click.style("<<Metrics>>", fg="green"))
     click.echo(f"Accuracy: {accuracy}")
     click.echo(f"F1: {f1}")
