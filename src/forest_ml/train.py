f92b5293039162dfe8a004d2f854fc55f6fd0b9f
dimakarpuk22 <dimakarpuk22@gmail.com>
2022-04-29 12:22:02 +0300

    Add CLI fuctional

--- src/forest_ml/train.py
+++ src/forest_ml/train.py
@@ -5,6 +5,8 @@ import click
 from joblib import dump
 
 import pandas as pd
+import numpy as np
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_validate
 
@@ -12,8 +14,8 @@ from sklearn.model_selection import cross_validate
 @click.option('-d', '--dataset-path', default='data/train.csv',
                     type=click.Path(exists=True, dir_okay=False,
                                                 path_type=Path))
-@click.optin('-s', '--save-model-path', default='data/model.joblib',
-                                            type=click.Path(exists=True, dir_okay=False,
+@click.option('-s', '--save-model-path', default='data/model.joblib',
+                                            type=click.Path(writable=True, dir_okay=False,
                                                 path_type=Path))
 @click.option(
     "--random-state",
@@ -34,16 +36,23 @@ from sklearn.model_selection import cross_validate
     show_default=True
 )
 @click.option(
-    "--min-sample_split",
+    "--min-samples_split",
     default=2,
     type=int,
     show_default=True
 )
 @click.option(
-    "--min-sample-leaf",
+    "--min-samples-leaf",
     default=2,
     type=int,
     show_default=True
+)
+@click.option(
+    "--key-metric",
+    default='accuracy',
+    type=str,
+    show_default=True,
+    help='Metric for select estimator'
 )
 # @click.option(
 #     "--cross-val-type",
@@ -51,7 +60,7 @@ from sklearn.model_selection import cross_validate
 #     type=str,
 #     show_default=True,
 #     help='Type of cross validation'
-)
+# )
 @click.option(
     "--cv",
     default=5,
@@ -65,14 +74,32 @@ def train(
     random_state: int,
     n_estimators: int,
     max_depth: int,
-    min_sample_split: int,
-    min_sample_leaf: int,    
-    cv: int
+    min_samples_split: int,
+    min_samples_leaf: int,    
+    cv: int,
+    key_metric: str
 ) -> None:
     df=pd.read_csv(dataset_path, index_col='Id')
     X = df.iloc[:,:-1]
     y = df.iloc[:, -1]
-    scorring = ('accuracy', 'f1', 'roc_auc')
+    scorring = ('accuracy', 'f1_weighted', 'roc_auc_ovr')
     clf = RandomForestClassifier(n_estimators=n_estimators, random_state=random_state, max_depth=max_depth,
-    min_sample_split=min_sample_split, min_sample_leaf=min_sample_leaf)
-    cv_results = cross_validate(clf, X, y, cv=cv, scoring=scorring)
+    min_samples_split=min_samples_split, min_samples_leaf=min_samples_leaf)
+    cv_results = cross_validate(clf, X, y, cv=cv, scoring=scorring, return_estimator=True)
+    accuracy = cv_results['test_accuracy'].max()
+    f1 = cv_results['test_f1_weighted'].max()
+    roc_auc = cv_results['test_roc_auc_ovr'].max()
+    if key_metric == 'accuracy':
+        estimator = cv_results['estimator'][np.argmax(cv_results['test_accuracy'])]
+    elif key_metric == 'f1':
+        estimator = cv_results['estimator'][np.armax(cv_results['test_f1_weighted'])]
+    elif key_metric == 'roc_auc':
+        estimator = cv_results['estimator'][np.argmax(cv_results['test_roc_auc_ovr'])]
+    click.echo(click.style('<<Metrics>>', fg='green'))
+    click.echo(f'Accuracy: {accuracy}')
+    click.echo(f'F1: {f1}')
+    click.echo(f'ROC_AUC: {roc_auc}')
+    click.echo(click.style('<<Model>>', fg='green'))
+    click.echo(f'Best model: {estimator}')
+    dump(estimator, save_model_path)
+    click.echo(f'Model is saved to {save_model_path}')
