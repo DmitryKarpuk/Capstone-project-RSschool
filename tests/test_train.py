9d6a80054f46eff39766ab435e83e2603998c756
dimakarpuk22 <dimakarpuk22@gmail.com>
2022-04-30 16:18:53 +0300

    Add simple test

---
+++ tests/test_train.py
@@ -0,0 +1,26 @@
+from click.testing import CliRunner
+import pytest
+
+from forest_ml.train import train
+
+
+@pytest.fixture
+def runner() -> CliRunner:
+    """Fixture providing click runner."""
+    return CliRunner()
+
+def test_error_for_invalid_cv(
+    runner: CliRunner
+) -> None:
+    ''' Fails when CV parameters less then 1 '''
+    result = runner.invoke(train, ['--cv', 0])
+    assert result.exit_code == 2
+    assert "Invalid value for '--cv'" in result.output    
+
+def test_cv(
+    runner: CliRunner
+) -> None:
+    ''' OK when CV parameters less then 1 '''
+    result = runner.invoke(train, ['--cv', 2])
+    assert result.exit_code == 0
+    assert "Valid value for '--cv'" in result.output  
