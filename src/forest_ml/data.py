9b9965082c83dfc00c24eb3925168bb9aeba63ee
dimakarpuk22 <dimakarpuk22@gmail.com>
2022-04-30 21:29:30 +0300

    Add fuctions for data processing

---
+++ src/forest_ml/data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from typing import Tuple, List
+
+
+def get_data(csv_path: Path) -> pd.DataFrame:
+    df = pd.read_csv(csv_path)
+    df.drop(columns="Id", inplace=True)
+    return df
+
+
+def get_split_data(
+    df: pd.DataFrame, features: List, target: List
+) -> Tuple[pd.DataFrame, np.ndarray]:
+    X = df.loc[:, features]
+    y = df.loc[:, target].values.ravel()
+    return X, y
