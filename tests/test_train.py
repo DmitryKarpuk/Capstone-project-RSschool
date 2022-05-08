be4c9d8f59bd4f591cbd7599bc7da7447053eabf
dimakarpuk22 <dimakarpuk22@gmail.com>
2022-05-08 17:27:58 +0300

    Add main tests

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,8 +1,15 @@
 from click.testing import CliRunner
+import click
 import pytest
+import os
+
 
 from forest_ml.train import train
 
+parent_path = os.getcwd()
+default_arg = ["-d", parent_path + r"\tests\test_data\pytest_train.csv",
+    "-p", parent_path + r"\tests\test_data\estimate_params.json",
+    "-st", "kfold_cv"]
 
 @pytest.fixture
 def runner() -> CliRunner:
@@ -11,14 +18,27 @@ def runner() -> CliRunner:
 
 
 def test_error_for_invalid_cv(runner: CliRunner) -> None:
-    """ Fails when CV parameters less then 1 """
-    result = runner.invoke(train, ["--cv", 0])
-    assert result.exit_code == 2
-    assert "Invalid value for '--cv'" in result.output
+    """Fails when CV parameters less then 1"""
+    with runner.isolated_filesystem():
+        path = os.getcwd()
+        arglist = default_arg + ["-s", path + '/model.joblib']
+        result = runner.invoke(train, arglist)
+    assert result.exit_code == 0, 'CV less then 0'
 
 
 def test_cv(runner: CliRunner) -> None:
-    """ OK when CV parameters less then 1 """
-    result = runner.invoke(train, ["--cv", 2])
+    """OK when CV parameters more then 1"""
+    with runner.isolated_filesystem():
+        path = os.getcwd()
+        arglist = default_arg + ["-s", path + '/model.joblib']
+        result = runner.invoke(train, arglist)
     assert result.exit_code == 0
-    assert "Valid value for '--cv'" in result.output
+
+def test_default_option(runner: CliRunner) -> None:
+    """OK if train works without error on default option"""
+    with runner.isolated_filesystem():
+        path = os.getcwd()
+        arglist = default_arg + ["-s", path + '/model.joblib']
+        result = runner.invoke(train, arglist)
+    assert result.exit_code == 0, 'Train fuction is faild'
+
