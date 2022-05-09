f69ce1ff22883b41768334104da7eabb862f3818
dimakarpuk22 <dimakarpuk22@gmail.com>
2022-05-09 10:56:33 +0300

    Optimized tets

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,5 +1,4 @@
 from click.testing import CliRunner
-import click
 import pytest
 import os
 
@@ -7,9 +6,15 @@ import os
 from forest_ml.train import train
 
 parent_path = os.getcwd()
-default_arg = ["-d", parent_path + r"\tests\test_data\pytest_train.csv",
-    "-p", parent_path + r"\tests\test_data\estimate_params.json",
-    "-st", "kfold_cv"]
+default_arg = [
+    "-d",
+    parent_path + r"\tests\test_data\pytest_train.csv",
+    "-p",
+    parent_path + r"\tests\test_data\estimate_params.json",
+    "-st",
+    "kfold_cv",
+]
+
 
 @pytest.fixture
 def runner() -> CliRunner:
@@ -21,24 +26,24 @@ def test_error_for_invalid_cv(runner: CliRunner) -> None:
     """Fails when CV parameters less then 1"""
     with runner.isolated_filesystem():
         path = os.getcwd()
-        arglist = default_arg + ["-s", path + '/model.joblib']
+        arglist = default_arg + ["-s", path + "/model.joblib", "--cv", "0"]
         result = runner.invoke(train, arglist)
-    assert result.exit_code == 0, 'CV less then 0'
+    assert result.exit_code == 0, "CV less then 2"
 
 
 def test_cv(runner: CliRunner) -> None:
     """OK when CV parameters more then 1"""
     with runner.isolated_filesystem():
         path = os.getcwd()
-        arglist = default_arg + ["-s", path + '/model.joblib']
+        arglist = default_arg + ["-s", path + "/model.joblib", "--cv", "2"]
         result = runner.invoke(train, arglist)
-    assert result.exit_code == 0
+    assert result.exit_code == 0, "Invalid cv parameter"
+
 
 def test_default_option(runner: CliRunner) -> None:
     """OK if train works without error on default option"""
     with runner.isolated_filesystem():
         path = os.getcwd()
-        arglist = default_arg + ["-s", path + '/model.joblib']
+        arglist = default_arg + ["-s", path + "/model.joblib"]
         result = runner.invoke(train, arglist)
-    assert result.exit_code == 0, 'Train fuction is faild'
-
+    assert result.exit_code == 0, "Train fuction is faild"
