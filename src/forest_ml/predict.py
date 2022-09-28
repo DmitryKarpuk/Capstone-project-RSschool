c19ee43413610bf3890ea6899d701f889ed86c82
unknown <dimakarpuk22@gmail.com>
2022-09-28 22:43:47 +0300

    Change predict script

--- src/forest_ml/predict.py
+++ src/forest_ml/predict.py
@@ -1,11 +1,21 @@
 import mlflow
 import click
 import pandas as pd
+import numpy as np
 from pathlib import Path
 from joblib import load
 from . import __version__
 from forest_ml.data import get_data
 
+def _predict(X: pd.DataFrame, model: str) -> np.array:
+    if Path(model).suffix == ".joblib":
+        # Load model from joblib file.
+        loaded_model = load(model)
+    else:
+        # Load model as a PyFuncModel.
+        loaded_model = mlflow.pyfunc.load_model(model)
+    pred = loaded_model.predict(X)
+    return pred
 
 @click.command()
 @click.version_option(version=__version__)
@@ -29,20 +39,13 @@ from forest_ml.data import get_data
     type=str,
 )
 def predict(dataset_path: Path, submission_path: Path, model: str) -> None:
-
-    if Path(model).suffix == ".joblib":
-        # Load model from joblib file.
-        loaded_model = load(model)
-    else:
-        # Load model as a PyFuncModel.
-        loaded_model = mlflow.pyfunc.load_model(model)
     # Load data
     data_df = get_data(dataset_path)
+    X = data_df.drop(columns="Id")
     # Predict on a Pandas DataFrame.
-    pred = loaded_model.predict(pd.DataFrame(data_df.drop(columns="Id")))
+    pred = _predict(X, model)
     submission = pd.DataFrame(data={"Id": data_df["Id"], "Cover_Type": pred})
     submission.to_csv(submission_path, index=False)
     click.echo(click.style("<<Model>>", fg="green"))
-    click.echo(f"Used model {loaded_model}")
     click.echo(click.style("<<Prediction>>", fg="green"))
     click.echo(f"Submission is saved to {submission_path}")
